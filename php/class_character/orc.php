6f1f470e1d627e90edae1b013da2d1ce65e6c1ad
Geekparade <david_da_silva0hotmail.fr>
2018-03-16 11:49:21 +0100

    maj de la class orc

--- php/class_character/orc.php
+++ php/class_character/orc.php
@@ -4,8 +4,7 @@ class Orc
 	protected $_name;
 	protected $_xp;	
 	protected $_life = 250;
-	protected $_strengthMin = 50;
-	protected $_strengthMax = 200;
+	protected $_strength = 200;
 	protected $_faction = 'Monster';
 	protected $_speciality = 'Warrior';	
 }
